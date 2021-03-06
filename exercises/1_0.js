2957c4a06077a8a945f7023e810b64ea4275bfa3
Marco David Martinez <marcod.martinez@softtek.com>
2021-03-05 23:24:18 -0600

    Create 1_0.js

---
+++ exercises/1_0.js
@@ -0,0 +1,39 @@
+// Exercise
+// Calculate Body mass index of the following people
+// Calcular el indice de masa corporal de las siguientes personas
+
+// Exepcted print to console the result of each person
+// Esperado imprimir el resultado en la consola de cada persona
+
+
+// 1- Usando variables, using cars
+const marcoHeight = 170; //cm
+const marcoWeight = 80;//kg
+
+const lilyHeight = 160; //cm
+const lilyWeight = 50;//kg
+
+// 
+// 2- using objects, usando objectos
+const marco = {
+    height: 170,
+    weight: 80,
+};
+const lily = {
+    height: 160,
+    weight: 50,
+};
+
+// 3- using arrays of objects
+const persons = [
+    {
+        name: 'marco',
+        height: 170,
+        weight: 80,
+    },
+    {
+        name: 'lily',
+        height: 160,
+        weight: 50,
+    }
+];
