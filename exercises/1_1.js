5681a4b4fcb8c91a5bef27069d8b64999f49b3cb
Marco David Martinez <marcod.martinez@softtek.com>
2021-03-05 23:29:34 -0600

    Added Exercise To create a function

---
+++ exercises/1_1.js
@@ -0,0 +1,15 @@
+// Exercise
+// Create A function that accepts 2 imputs, height and weight to calculate body index
+// Crear una function que accepte 2 parametros; altura y peso para calcular el indice de masa corporal
+
+// Exepcted return body index then print the result to the console
+// Esperado regresar el indice de masa corporal e imprimirlo a la consola
+
+
+/**
+ * Returns body index
+ *
+ * @param {number} height The person height in cm
+ * @param {number} weight The person weight in kg
+ * @return {number} index body index
+ */
